3bc30d1c799626ce19c0a0408c2719bd3eaaa809
Junaid Shaikh <shekhjuned111@gmail.com>
2022-12-16 17:25:59 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,25 @@
 
+<?php
+class School{
+
+	public $a, $b, $c;
+
+		function Sum(){
+			$this->c = $this->a + $this->b;
+			return $this->c;
+		}
+
+		function Sub(){
+			$this->c = $this->a - $this->b;
+			return $this->c;
+		}
+}
+$c1 = new School();
+
+$c1->a = 50;
+$c1->b = 500;
+
+echo $c1->Sum();
+echo "<br>";
+echo $c1->Sub();
+?>
